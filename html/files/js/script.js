1c95ae3abefadc59abf609d763edd9041da04e70
Michele Winkelman <mlwinkelman@gmail.com>
2017-05-04 23:35:57 -0600

    added services page

--- html/files/js/script.js
+++ html/files/js/script.js
@@ -472,6 +472,18 @@ jQuery(window).load(function() {
 		return false;
 	});
 
+	/*----------------------------------------------
+	Page scroll alternative
+	----------------------------------------------*/
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+      // $('a.page-scroll').bind('click', function(event) {
+      //   var $anchor = $(this);
+      //   $('html, body').stop().animate({
+      //     scrollTop: ($($anchor.attr('href')).offset().top - 5)
+      //   }, 1250, 'easeInOutExpo');
+      //   event.preventDefault();
+      // });
+
 
 
 	/*----------------------------------------------
